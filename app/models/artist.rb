23e997b4a99881ee916f25d709c57229d7e1addf
alanbanks229 <alanbanks229@gmail.com>
2020-02-06 23:14:16 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
+#an artist has many songs
+#an artist has many genres through songs.
+#below we are essentially typing macros
 class Artist < ActiveRecord::Base
+    has_many :songs
+    has_many :genres, through: :songs
 end
